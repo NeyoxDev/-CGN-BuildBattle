5c0c265f7cbc93771a42d529d0547509d402730f
NeyoxDev <40002265+NeyoxDev@users.noreply.github.com>
2020-08-11 21:35:28 +0200

    Update BlockArea.java

--- [CGN] BuildBattle/src/fr/neyox/bb/utils/BlockArea.java
+++ [CGN] BuildBattle/src/fr/neyox/bb/utils/BlockArea.java
@@ -3,7 +3,6 @@ package fr.neyox.bb.utils;
 import org.bukkit.Location;
 import org.bukkit.block.Block;
 
-//Funcraft lol
 public interface BlockArea extends Iterable<Block>, Cloneable {
 	
 	boolean contains(Block paramBlock);
